4dfbc06e3f03d2ed4e29647fa9bfdc9cdf078799
Vlad <vs556@bath.ac.uk>
2022-07-26 00:41:41 +0100

    Finalised labeller.
    Labeller finalised. Added a label_my_files script to label custom files

--- openvibe_output_labeller/labeller.py
+++ openvibe_output_labeller/labeller.py
@@ -4,7 +4,12 @@ import warnings
 
 
 class Labeller:
-    def __init__(self, sequences=None, openvibe_output_path=None):
+    def __init__(self, sequences_list=None, input_path=None, save_dir=None):
+        """
+        :param sequences_list: List of sequence pathlib Paths
+        :param input_path: string path to the folder which needs to be recursively searched for csv files to label
+        :param save_dir: string path to the folder where to save the labelled data
+        """
         # use dictionary to match OpenVibe's Stimulation ID labels to phone labels
         self.labels = {"OVTK_StimulationId_Label_01": "p",
                        "OVTK_StimulationId_Label_02": "t",
@@ -24,22 +29,30 @@ class Labeller:
                        "OVTK_StimulationId_Label_10": "thought"
                        }
 
-        if sequences is None:
+        if sequences_list is None:
             raise ValueError("Please provide the sequences files.")
         else:
-            self.sequences_dirs = sequences
+            self.sequences_dirs = sequences_list
         print(f"Using sequences {self.sequences_dirs}")
 
         # set the path to directory for the csv files output from OpenVibe
-        if openvibe_output_path is None:
-            self.directory = Path("../output/")
+        if input_path is None:
+            raise ValueError("Please provide directory which you want to search for csv files to be labelled.")
         else:
-            self.directory = Path(openvibe_output_path)
+            self.directory = Path(input_path)
         print(f"Labelling files in directory {self.directory} and its child directories")
+
+        if save_dir is None:
+            raise ValueError("Please provide directory for saving labelled csv files.")
+        else:
+            self.save_dir = save_dir
+        print(f"Saving files to directory {self.save_dir} \n")
 
     def label_csvs(self):
         # keep in mind that this code will read all csv files in the chosen directory and its children
         # so make sure no other csv files exist in the chosen directory
+        only_one_sequence_file = False
+        sequences_dict = self.open_sequence_files()
         file_number = 0
         for file in self.directory.rglob('*.csv'):
             file_df = pd.read_csv(file)  # create pandas dataframe
@@ -53,8 +66,6 @@ class Labeller:
             file_df['Label'] = ""
             file_df['Stage'] = file.stem
 
-            sequences_dict = self.open_sequence_files()
-
             # choose sequence based on the name of the parent directory of the current csv file
             if 'imagined' in str(file.parent):
                 index = int(str(file.parent)[-1])
@@ -63,8 +74,11 @@ class Labeller:
                 index = int(str(file.parent)[-1])
                 sequence = sequences_dict[f'sequence_inner_{index}']
             else:
-                raise ValueError(f"Unidentified file path {file.parent}. "
-                                 f"Does not contain reference to imagined or inner.")
+                print("Filepath does not contain 'imagined' or 'inner'. "
+                      "Assuming that only a single sequence file is being used to label. "
+                      "Using first sequence in dictionary.\n")
+                only_one_sequence_file = True
+                sequence = list(sequences_dict.values())[0]
 
             print(f"Labelling file {file_number}: {file}")
             print(f"Using sequence: {sequence}")
@@ -74,11 +88,14 @@ class Labeller:
                 file_df.loc[file_df['Epoch'] == epoch, 'Label'] = self.labels[sequence[epoch]]
 
             # save labelled files in the chosen save path
-            save_path = Path("../raw_eeg_recordings_labelled/" + str(file.parent).strip('..\\raw_eeg_recordings'))
+            save_path = Path(self.save_dir + str(file.parent).strip('..\\raw_eeg_recordings'))
+            if only_one_sequence_file:
+                save_path = Path(self.save_dir + str(file.parent))
+
             if not save_path.is_dir():
                 save_path.mkdir(parents=True)
             file_df.to_csv(str(save_path) + '/' + file.stem + "_labelled.csv", index=False)  # save dataframe as csv
-            print(f"File number {file_number} saved in: {save_path}")
+            print(f"File number {file_number} saved in: {save_path}\n")
             file_number += 1
 
     def open_sequence_files(self):
@@ -108,5 +125,6 @@ if __name__ == '__main__':
     sequence_paths = [sequence_imagined_0, sequence_imagined_1, sequence_imagined_2,
                       sequence_inner_0, sequence_inner_1, sequence_inner_2]
 
-    labeler = Labeller(sequences=sequence_paths, openvibe_output_path="../raw_eeg_recordings/")
-    labeler.label_csvs()
+    labeller = Labeller(sequences_list=sequence_paths, input_path="../raw_eeg_recordings/",
+                        save_dir="../raw_eeg_recordings_labelled/")
+    labeller.label_csvs()
