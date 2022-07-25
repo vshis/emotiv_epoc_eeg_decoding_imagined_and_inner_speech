1feea72568cd8814f281dd0af12d38aa2be643f4
Vlad <vs556@bath.ac.uk>
2022-07-26 00:45:33 +0100

    Small edits to label_my_files

--- openvibe_output_labeller/label_my_files.py
+++ openvibe_output_labeller/label_my_files.py
@@ -3,7 +3,13 @@ from pathlib import Path
 
 
 def label_my_file():
-    """ See the files in the sample_data_for_label_my_files_script folder for examples of data used """
+    """
+    See the files in the sample_data_for_label_my_files_script folder for examples of data used
+    To label your own files:
+    1. Set the sequence_path variable to your .txt sequence used in timeline-generator.lua
+    2. Set the input_files_path variable to the folder, to recursively search through for csv files to label
+    3. Set the save_path variable to the folder, to save the labelled data in
+    """
     # select path to the txt of sequence used in experiment-timeline.lua script that you want to use to label your data
     sequence_path = 'sample_data_for_label_my_files_script/sequence.txt'  # EDIT THIS
     # select directory to recursively search in for csv files to label
