bfa6a4d7062e6fe978820cf39a4257ccdae7a6e9
Vlad <vs556@bath.ac.uk>
2022-07-27 22:10:54 +0100

    Epoching using MNE
    So far the Epoched Frequency domain graph looks different for some reason when epoching...

--- data_processing/mne_tests.py
+++ data_processing/mne_tests.py
@@ -22,7 +22,7 @@ def load_data(filepath: str, verbose=False, data_type='feis'):
         channels_df = df.drop(labels=['Time:256Hz', 'Epoch', 'Label', 'Stage', 'Flag'], axis=1)
         sfreq = 256  # Hz
     elif data_type == 'my':  # my data LABELED
-        channels_df = df.drop(labels=['Time:128Hz', 'Epoch', 'Event Id', 'Event Date', 'Event Duration'], axis=1)
+        channels_df = df.drop(labels=['Time:128Hz', 'Epoch', 'Label', 'Stage'], axis=1)
         sfreq = 128  # Hz
     else:
         return
@@ -46,15 +46,36 @@ def load_data(filepath: str, verbose=False, data_type='feis'):
     return raw
 
 
+def create_epochs_object(mne_raw_object: mne.io.RawArray, filepath: str):
+    events = mne.make_fixed_length_events(mne_raw_object, start=0, duration=5.0)
+    epochs_mne = mne.Epochs(mne_raw_object, events)
+    df = pd.read_csv(Path(filepath))
+    labels = [df.loc[df['Epoch'] == epoch, 'Label'].iloc[0] for epoch in range(df['Epoch'].max()+1)]
+    epochs_mne.metadata = pd.DataFrame(data=labels, columns=['Labels'], index=range(len(labels)))
+    return epochs_mne
+
+
 if __name__ == '__main__':
-    data_mne = load_data('testing_data/thinking.csv', verbose=True)
-    raw_data = data_mne.copy()
-    ica = mne.preprocessing.ICA(n_components=14, random_state=69, max_iter=800)
-    data_mne.filter(l_freq=1., h_freq=None)
-    data_mne.notch_filter(50)
-    #data_mne.plot_psd()
-    ica.fit(data_mne, picks='eeg')
-    ica.exclude = ['AF3', 'AF4']
-    ica.apply(data_mne)
+    filepath = 'testing_data/thinking.csv'
+    data_mne = load_data(filepath, verbose=False, data_type='feis')
+    #raw_data = data_mne.copy()
+    create_epochs_object(data_mne, filepath)
+
+
     data_mne.plot(block=True, scalings='auto')
-    raw_data.plot(block=True, scalings='auto')
+    data_mne.plot_psd()
+    epochs_mne.plot(scalings='auto')
+    epochs_mne.plot_psd()
+
+    #print(mne.find_events(data_mne, shortest_event=1, initial_event=True, stim_channel='F3'))
+    #ica = mne.preprocessing.ICA(n_components=14, random_state=69, max_iter=800)
+    #data_mne.filter(l_freq=1., h_freq=None)
+    #data_mne.notch_filter(50)
+    #data_mne.plot_psd()
+    #ica.fit(data_mne, picks='eeg')
+    #ica.exclude = ['AF3', 'AF4']
+    #ica.apply(data_mne)
+    #raw_data.plot(block=True, scalings='auto')
+    #raw_data.plot_psd()
+
+
